161cc96ddcf0553e17797573a7b6307a2410a62e
Kasperovich Artem <kaspiarovich.artsemi@gmail.com>
2022-04-26 18:11:19 +0300

    done

--- kyu5/The_Hashtag_Generator.py
+++ kyu5/The_Hashtag_Generator.py
@@ -1,1 +1,9 @@
-1
+"""
+https://www.codewars.com/kata/52449b062fb80683ec000024/train/python
+"""
+
+
+def generate_hashtag(s):
+    if 0 < len(s) < 140:
+        return '#' + s.title().replace(' ', '')
+    return False
