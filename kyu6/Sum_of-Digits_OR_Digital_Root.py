cd42f45061586497d66b3454f7c36271f38029f3
Kasperovich Artem <kaspiarovich.artsemi@gmail.com>
2022-04-26 18:35:22 +0300

    done

---
+++ kyu6/Sum_of-Digits_OR_Digital_Root.py
@@ -0,0 +1,11 @@
+"""
+https://www.codewars.com/kata/541c8630095125aba6000c00/train/python
+"""
+
+from functools import reduce
+
+
+def digital_root(n):
+    if n < 10:
+        return n
+    return digital_root(reduce(lambda x, y: int(x) + int(y), str(n)))
